78df52db496dba86f33d810739b9211cc5e79884
rufasa85 <rufasa85@gmail.com>
2018-02-26 12:12:58 -0800

    simple routing

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ app.get('/', function (req, res) {
 	res.render('index')
 });
 
+app.get('/plays', function(req, res) {
+	res.render('plays/index')
+});
+
+app.get('/games', function(req, res) {
+	res.render('games/index')
+});
+
+app.get('/games/:id', function(req,res) {
+	res.render('games/show', {game:req.params.id})
+})
+
+app.get('/about', function(req, res) {
+	res.render('about')
+});
+
 app.listen(3000);
